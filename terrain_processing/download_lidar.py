579b543b18d2d87e92ccf8473e309388d2e0173e
samlevin11 <samlevin11@gmail.com>
2024-11-05 18:22:05 -0700

    Terrain processing scripting changed to standard python files

---
+++ terrain_processing/download_lidar.py
@@ -0,0 +1,25 @@
+import os
+import requests
+import time
+
+start = time.perf_counter()
+
+# Create a directory to download the LAZ file to
+data_dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '/../data')
+print(f'creating data directory: {data_dir}')
+os.mkdir(data_dir)
+
+# Metadata links
+# https://www.sciencebase.gov/catalog/item/62b6b4a2d34e8f4977cc320a
+# https://prd-tnm.s3.amazonaws.com/index.html?prefix=StagedProducts/Elevation/metadata/WY_SouthCentral_2020_D20/WY_SouthCentral_3_2020
+laz_url = 'https://rockyweb.usgs.gov/vdelivery/Datasets/Staged/Elevation/LPC/Projects/WY_SouthCentral_2020_D20/WY_SouthCentral_3_2020/LAZ/USGS_LPC_WY_SouthCentral_2020_D20_13TDF670640.laz'
+
+laz_path = os.path.join(data_dir, os.path.split(laz_url)[1])
+print(f'downloading to local file: {laz_path}')
+
+# Use request library to retrieve and write the file to local data 
+resp = requests.get(laz_url)
+with open(laz_path, 'wb') as file:
+    file.write(resp.content)
+
+print(f'DOWNLOAD TIME: {round(time.perf_counter() - start)} seconds')
---
+++ terrain_processing/lidar_to_terrain_rasters.py
@@ -0,0 +1,69 @@
+import os
+import time
+import whitebox
+import rasterio
+from rasterio.crs import CRS
+
+start = time.perf_counter()
+
+wbt = whitebox.WhiteboxTools()
+
+# Set data directory where LAZ was previously downloaded
+data_dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '/../data')
+print(f'setting WBT working directory: {data_dir}')
+wbt.set_working_dir(data_dir)
+
+# Use IDW interpolation to create a raw DEM from the LAZ
+# Only include LAST returns, class 2 points (ground)
+# 1 meter resolution
+print('interpolating raw DEM LAZ')
+laz_file = 'USGS_LPC_WY_SouthCentral_2020_D20_13TDF670640.laz'
+raw_dem = os.path.splitext(laz_file)[0] + '_RawDEM.tif'
+wbt.lidar_idw_interpolation(
+    i=laz_file,
+    output=raw_dem,
+    parameter="elevation",
+    returns="last",
+    exclude_cls='0,1,3-18',
+    resolution=1.0,
+    weight=1.0,
+    radius=2.5
+)
+print(raw_dem)
+
+print('defining DEM coordinate reference system')
+# EPSG:6342 (NAD83(2011) / UTM zone 13N)
+crs = CRS.from_epsg(6342)
+with rasterio.open(os.path.join(data_dir, raw_dem), 'r+') as dem:
+    dem.crs = crs
+
+print('filling nodata holes')
+# Fill NoData holes in the DEM 
+filled_dem = os.path.splitext(laz_file)[0] + '_FilledDEM.tif'
+wbt.fill_missing_data(
+    i=raw_dem,
+    output=filled_dem,
+    filter=20
+)
+print(filled_dem)
+
+print('calculating slope')
+# Calculate Slope using filled DEM
+slope_rast = os.path.splitext(laz_file)[0] + '_Slope.tif'
+wbt.slope(
+    dem=filled_dem,
+    output=slope_rast,
+    units='degrees'
+)
+print(slope_rast)
+
+print('calculating aspect')
+# Calculate Aspect using filled DEM
+aspect_rast = os.path.splitext(laz_file)[0] + '_Aspect.tif'
+wbt.aspect(
+    dem=filled_dem,
+    output=aspect_rast
+)
+print(aspect_rast)
+
+print(f'TERRAIN PROCESSING TIME: {round(time.perf_counter() - start)} seconds')
