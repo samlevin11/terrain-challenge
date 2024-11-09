2111e084478895314e5e4355d03089632aac18d9
samlevin11 <samlevin11@gmail.com>
2024-11-08 17:06:23 -0700

    PostGIS raster clip functions created
    Individual functions to clip each terrain raster (DEM, slope, aspect)
    Final function to call all three at once
    Returned as a table with all three clipped rasters as GeoTIFFs
    
    sql directory mounted as volume
    Scripts created via command line with psql

--- enable_extensions.sql
+++ enable_extensions.sql
@@ -1,1 +1,2 @@
 CREATE EXTENSION IF NOT EXISTS postgis_raster;
+ALTER DATABASE terrain SET postgis.gdal_enabled_drivers TO 'ENABLE_ALL';
---
+++ sql/func_clip_terrain.sql
@@ -0,0 +1,131 @@
+-- Clip filled DEM raster based on an input Area of Interest GeoJSON string
+-- Results returned as a raster
+CREATE OR REPLACE FUNCTION clip_dem(
+    -- Accept GeoJSON text as input
+    -- Defines the Area of Interest to clip terrain
+    aoi_geojson TEXT
+)
+-- Return a PostGIS raster
+RETURNS RASTER AS $$
+
+DECLARE 
+    dem_rast RASTER; 
+
+BEGIN
+
+SELECT
+-- Clip the DEM raster based on the GeoJSON geometry
+    ST_Clip(
+        filleddem.rast,
+        -- Project the Geometry to the same SRID as the raster before clipping
+        ST_Transform(
+            ST_GeomFromGeoJSON(aoi_geojson),
+            ST_SRID(filleddem.rast)
+        )
+    ) as dem_rast 
+INTO dem_rast
+FROM
+    public.filleddem;
+
+RETURN dem_rast; 
+   
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Clip slope raster based on an input Area of Interest GeoJSON string
+-- Results returned as a raster
+CREATE OR REPLACE FUNCTION clip_slope(
+    -- Accept GeoJSON text as input
+    -- Defines the Area of Interest to clip terrain
+    aoi_geojson TEXT
+)
+-- Return a PostGIS raster
+RETURNS RASTER AS $$
+
+DECLARE 
+    slope_rast RASTER; 
+
+BEGIN
+
+SELECT
+-- Clip the slope raster based on the GeoJSON geometry
+    ST_Clip(
+        slope.rast,
+        -- Project the Geometry to the same SRID as the raster before clipping
+        ST_Transform(
+            ST_GeomFromGeoJSON(aoi_geojson),
+            ST_SRID(slope.rast)
+        )
+    ) as slope_rast 
+INTO slope_rast
+FROM
+    public.slope;
+
+RETURN slope_rast; 
+   
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Clip aspect raster based on an input Area of Interest GeoJSON string
+-- Results returned as a raster
+CREATE OR REPLACE FUNCTION clip_aspect(
+    -- Accept GeoJSON text as input
+    -- Defines the Area of Interest to clip terrain
+    aoi_geojson TEXT
+)
+-- Return a PostGIS raster
+RETURNS RASTER AS $$
+
+DECLARE 
+    aspect_rast RASTER; 
+
+BEGIN
+
+SELECT
+-- Clip the aspect raster based on the GeoJSON geometry
+    ST_Clip(
+        aspect.rast,
+        -- Project the Geometry to the same SRID as the raster before clipping
+        ST_Transform(
+            ST_GeomFromGeoJSON(aoi_geojson),
+            ST_SRID(aspect.rast)
+        )
+    ) as aspect_rast 
+INTO aspect_rast
+FROM
+    public.aspect;
+
+RETURN aspect_rast; 
+   
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Use prevously creatd funtions to clip all three terrain rasters at once
+-- Convert results to GeoTIFF using ST_AsTIFF, as byte array
+-- Results of all three clipping operations returned as a table with three columns
+-- One column for each clipped raster (DEM, slope, aspect)
+CREATE OR REPLACE FUNCTION clip_terrain(
+    aoi_geojson TEXT
+)
+-- Return table with all three clipped rasters as GeoTIFFs (byte array)
+RETURNS TABLE (
+    dem_tiff BYTEA,
+    slope_tiff BYTEA,
+    aspect_tiff BYTEA
+) AS $$
+
+BEGIN
+RETURN QUERY
+SELECT
+-- Convert PostGIS rasters to GeoTIFF
+-- Ensure GDAL drivers are enabled 
+-- (configured in container startup, enable_extensions.sql)
+    ST_AsTIFF(clip_dem(aoi_geojson)),
+    ST_AsTIFF(clip_slope(aoi_geojson)),
+    ST_AsTIFF(clip_aspect(aoi_geojson));
+   
+END;
+$$ LANGUAGE plpgsql;
